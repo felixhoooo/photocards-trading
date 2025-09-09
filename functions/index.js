9955fd7b875322cbe47fb608662653db9c22311d
felixhoooo <felixhoooo@gmail.com>
2025-09-09 14:28:36 +0000

    remove admin logic

--- functions/index.js
+++
@@ -1,9 +0,0 @@
-const functions = require("firebase-functions");
-const admin = require("firebase-admin");
-admin.initializeApp();
-
-exports.setAdminClaim = functions.auth.user().onCreate(async (user) => {
-  if (user.email === "felixhoooo@gmail.com") {
-    await admin.auth().setCustomUserClaims(user.uid, { admin: true });
-  }
-});
